65657f2e7c1bf17d98f2cbf2242df33b06ed0384
Ross King <ross.king@altavec.com>
2024-08-21 17:07:34 +1000

    🎉 initial commit of aspire hosting components

---
+++ src/Aspire.Hosting.Grpc/GrpcUIContainerImageTags.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GrpcUIContainerImageTags.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Aspire.Hosting.Grpc;
+
+/// <summary>
+/// The <c>grpcui</c> container image tags.
+/// </summary>
+internal static class GrpcUIContainerImageTags
+{
+    /// <summary>
+    /// The registry.
+    /// </summary>
+    public const string Registry = "docker.io";
+
+    /// <summary>
+    /// The image.
+    /// </summary>
+    public const string Image = "fullstorydev/grpcui";
+
+    /// <summary>
+    /// The tag.
+    /// </summary>
+    public const string Tag = "latest";
+}
