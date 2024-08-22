371b6deb567aeaf42b4b5927e3bac2dca025a8c8
Ross King <ross.king@altavec.com>
2024-08-22 11:16:15 +1000

    ✨ add `grpc` playground

---
+++ playground/grpc/Grpc.AppHost/Program.cs
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.Grpc_ApiService>("grpc-apiservice", Uri.UriSchemeHttp)
+    .WithGrpcUI();
+
+await builder.Build().RunAsync().ConfigureAwait(false);
