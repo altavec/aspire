b63e05454e2718f9bda5b336543cf52759de58fb
Ross King <ross.king@altavec.com>
2024-10-28 12:47:59 +1100

    🧑‍💻 return PostGIS resource

---
+++ src/Aspire.Hosting.PostGIS/PostGisServerResource.cs
@@ -0,0 +1,12 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PostGisServerResource.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// A resource that represents a PostGIS container.
+/// </summary>
+public class PostGisServerResource(string name, ParameterResource? userName, ParameterResource password) : ApplicationModel.PostgresServerResource(name, userName, password);
