79542a9ad3149328b785a73f06d078ac27fe77a1
Ross King <ross.king@altavec.com>
2024-11-26 13:37:39 +1100

    ✨ allow EXE resource for grpcui

---
+++ src/Aspire.Hosting.Grpc/GrpcUIExecutableResource.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GrpcUIExecutableResource.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// Represents a executable resource for <c>GrpcUI</c>.
+/// </summary>
+/// <param name="name">The name of the executable resource.</param>
+/// <exception cref="InvalidOperationException">Could not find the gRPC executable.</exception>
+public sealed class GrpcUIExecutableResource(string name) : ExecutableResource(name, GetToolPath(), string.Empty)
+{
+    private static string GetToolPath() => Altavec.Runtime.Resolve.Tool(Altavec.Runtime.InteropServices.RuntimeEnvironment.CreateExecutableName("grpcui")) ?? throw new InvalidOperationException("Failed to find the grpcui executable");
+}
